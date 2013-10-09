d21ada25be146fb529eccdf751e3ebbc73a41a32
Ricardo Parro <ricardo.parro@gmail.com>
2013-10-09 18:05:47 -0300

    almost done for vero

--- Vero/orderconfirmation.js
+++ Vero/orderconfirmation.js
@@ -27,84 +27,41 @@ if (document.getElementById("dataLayer")) {
     console.log(jsonCant);
     console.log(jsonCant.length);
     if (jsonCant.length == 3) {
-      jsonUser = jsonUser.replace(/var/g, "").replace(/\s/g, "").replace(/[\[\]&]+/g, '');
+      jsonUser = jsonUser.replace(/var/g, "").replace(/\s/g, "");
       
       console.log("jsonUSer ====> " + jsonUser);
       var pos = jsonUser.indexOf('}');
       console.log("position ==============> " + pos);
 
-      var transactionsAux = jsonUser.substring(0, pos +1).split('=');
+      var transactionsAux = jsonUser.substring(0, pos + 2).split('=');
       var transactionTotalOrderAux = jsonUser.substring(pos, (jsonUser.length - 1)).split('=');
 
-      var transactions = $.parseJSON( trimComma(transactionsAux[1]));
-      var transactionTotalOrder = $.parseJSON(trimComma(transactionTotalOrderAux[1]));
-      debugger;
+      console.log(transactionsAux[1]);
+      var transactionTotalOrder = $.parseJSON( trimComma(transactionsAux[1]));
+      var transactions = $.parseJSON(trimComma(transactionTotalOrderAux[1]));
+
       console.log("transactions ==============> " + transactions);
       console.log("transactionTotalOrder ==============> " + transactionTotalOrder);
+      var itemsCart = new Array();
 
+      $.each(transactions, function(index, value) {
 
-   //   var matches = jsonUser.split('}]');
-  //    console.log("matches ====> " + matches);
-     // if (matches) {
+        console.log("city ==============> " + value.city);
 
+        itemsCart.push({
+          "total_amount": value.orderValue,
+          "shipping_cost": value.shippingValue,
+          "payment_method": value.paymentMethod,
+          "transaction_id": value.orderID,
+          "product_category": value.productName,
+          "quantity": value.quantity,
+          "city": value.city
+        });
+      });
 
-
-        // $.each(matches, function(index, value) {
-
- //       console.log("matches 0 ===========================================>" + matches[0]);
-
-//console.log("matches 1 ===========================================>" + matches[1]);
-
-        //   var submatch = matches[index];
-        //   console.log("submatch ====> " + submatch);  
-        //   var arrayVariables = JSON.parse(submatch);
-        //   console.log("arrayVariables ====> " + arrayVariables);  
-          
-        //   $.each(arrayVariables, function(key, val) {
-        //     console.log("type ============================> " + typeof val);
-        //     if(typeof val == "object"){
-        //       variables["transactions"] = val;
-        //     }
-        //     else{
-        //       variables[key] = val;
-        //     }
-
-        //     console.log("key ---->" + key);
-        //     console.log("val ----->" + val);
-
-          
-            
-
-        //   }); 
-        // });
-
-   //     console.log("final ===========================================>" + variables["transactions"]);
-
-
-     // $.each(variables["transactions"], function(i, v) {
-
-     //    console.log("key ---->" + i);
-     //    console.log("val ----->" + v);
-
-     // });
-
-      // $.each(cartTotalOrder, function(key, val) {
-      //   console.log("key ---->" + key);
-      //   console.log("val ----->" + val.orderValue);
-      //   itemsCart.push({
-      //     "total_amount": val.orderValue,
-      //     "shipping_cost": val.shippingValue,
-      //     "payment_method": val.paymentMethod,
-      //     "transaction_id": val.orderID,
-      //     "product_category": val.productName,
-      //     "quantity": val.quantity,
-      //     "city": val.city
-      //   });
-       //});
-/*      window._veroq.push(['track', 'Successful Purchase', {
-        "cart": itemsCart
-      }]); */
- // }
+      // window._veroq.push(['track', 'Successful Purchase', {
+      //   "cart": transactions
+      // }]);
 }}
 else{
     console.log("set timeout ====> will check in 1 sec");
